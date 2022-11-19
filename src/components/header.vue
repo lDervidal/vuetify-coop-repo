1b12ab5dd4841b52513c9d62176858c79273cc50
lDervidal <105472407+lDervidal@users.noreply.github.com>
2022-11-19 17:06:56 +0300

    test

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,9 +1,50 @@
 <template>
     <v-container>
-        хедер
+        <v-row justify="space-between" class="text-center">
+            <v-col>
+                <v-btn icon="mdi-heart" color="primary"></v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn icon="mdi-star" color="secondary"></v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn icon="mdi-cached" color="info"></v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn icon="mdi-thumb-up" color="success"></v-btn>
+            </v-col>
+        </v-row>
+
+        <v-row justify="space-between" class="text-center">
+            <v-col>
+                <v-btn icon color="primary">
+                    <v-icon>mdi-heart</v-icon>
+                </v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn icon color="secondary">
+                    <v-icon>mdi-star</v-icon>
+                </v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn icon color="info">
+                    <v-icon>mdi-cached</v-icon>
+                </v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn icon color="success">
+                    <v-icon>mdi-thumb-up</v-icon>
+                </v-btn>
+            </v-col>
+        </v-row>
     </v-container>
-  </template>
-  
-  <script setup>
-  </script>
+</template>
+<script setup>
+</script>
   
