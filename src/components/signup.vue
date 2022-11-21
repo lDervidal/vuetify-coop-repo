24b132260c92b1052735bd740716900282710502
pwrshi <stasonukesweet@gmail.com>
2022-11-21 23:00:20 +0300

    add signup

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,9 +1,22 @@
 <template>
-    <v-container>
-        сайнап
+    <v-container fluid class="align-center justify-center d-flex pt-16 pb-16" style="background-color: #F5F5F5">
+        <div class="align-right w-50">
+            <h3 class="text-h4 font-weight-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore</h3>
+            </div>
+        <div>
+            <v-btn color="secondary" class="text-white ml-8  mr-4 py-1 px-8 button-text" size="large" rounded="lg">Join Today</v-btn>
+            <v-btn variant="outlined" color="secondary" class="py-1 px-8 button-text" size="large" rounded="lg">Contact us</v-btn>
+        </div>
     </v-container>
-  </template>
-  
-  <script setup>
-  </script>
+</template>
+
+<style scoped>
+.button-text {
+    font-size: 12px;
+}
+</style>
+
+<script setup>
+</script>
   
