233edc1af52776bbef2e2d5981b259327d78744d
lDervidal <105472407+lDervidal@users.noreply.github.com>
2022-11-21 22:16:53 +0300

    soasa

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -1,9 +1,22 @@
 <template>
-    <v-container>
-        херо
+    <v-container fluid class="d-flex align-center cnt flex-column">
+        <v-btn size="x-large" icon="mdi-play" color="blue"></v-btn>
+        <h1 class="mt-6 pb-6 text-center text-h4 text-white">The largest cummunity of photo enthusiasts</h1>
+        <v-btn color="blue" >
+            Join today
+        </v-btn>
     </v-container>
-  </template>
-  
-  <script setup>
-  </script>
+
+</template>
+
+<script setup>
+
+</script>
+<style>
+.cnt { background-image: url("https://cdn.vuetifyjs.com/images/parallax/material.jpg");
+padding-top: 150px;
+    height: 500px;
+    justify-content: center;
+}
+</style>
   
