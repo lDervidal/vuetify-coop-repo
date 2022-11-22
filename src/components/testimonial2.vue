c27880a3fa28b33325c66eff513cd3a769073964
Ivan-profan <61698771+ivan-profun@users.noreply.github.com>
2022-11-22 04:00:55 +0300

    add testimonial2

--- src/components/testimonial2.vue
+++ src/components/testimonial2.vue
@@ -1,7 +1,48 @@
 <template>
-    <v-container>
-        тестимониал2
-    </v-container>
+    <div class="pa-16">
+        <div>
+            <p class="text-h4 text-md-center pb-6">You’re in good company</p>
+        </div>
+        <div class="text-md-center pb-6">
+            <p class="d-inline-flex text-h5 font-weight-regular w-75">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+        </div>
+        <div>
+            <v-row>
+                <v-col>
+                    <div class="pa-8">
+                        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" width="175" height="75"></v-img>
+                    </div>
+                    <div class="pa-8">
+                        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" width="175" height="75"></v-img>
+                    </div>
+                </v-col>
+                <v-col>
+                    <div class="pa-8">
+                        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" width="175" height="75"></v-img>
+                    </div>
+                    <div class="pa-8">
+                        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" width="175" height="75"></v-img>
+                    </div>
+                </v-col>
+                <v-col>
+                    <div class="pa-8">
+                        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" width="175" height="75"></v-img>
+                    </div>
+                    <div class="pa-8">
+                        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" width="175" height="75"></v-img>
+                    </div>
+                </v-col>
+                <v-col>
+                    <div class="pa-8">
+                        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" width="175" height="75"></v-img>
+                    </div>
+                    <div class="pa-8">
+                        <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" width="175" height="75"></v-img>
+                    </div>
+                </v-col>
+            </v-row>
+        </div>
+    </div>
 </template>
   
 <script setup>
